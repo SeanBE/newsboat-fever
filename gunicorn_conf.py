30a7d3885f979999537959c186bc3526b055efbc
seanBE <SeanBE@users.noreply.github.com>
2022-09-27 23:24:15 +0100

    use a single gunicorn worker to avoid lockfile clashing

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -6,3 +6,4 @@ port = os.getenv("FEVER_PORT", "5000")
 wsgi_app = "api:app"
 bind = f"{host}:{port}"
 loglevel = os.getenv("FEVER_LOG_LEVEL", "INFO")
+workers = 1  # whilst we use the newsboat lockfile approach
